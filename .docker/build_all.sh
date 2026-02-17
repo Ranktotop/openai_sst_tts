61493ca7348a825f76d41a96063747cf85adda04
Ranktotop <marc@rank-to-top.de>
2026-02-17 16:48:01 +0100

    update

--- .docker/build_all.sh
+++ .docker/build_all.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-"$SCRIPT_DIR/build-gpu.sh" "$@"
-"$SCRIPT_DIR/build-cpu.sh" "$@"
+"$SCRIPT_DIR/build_gpu.sh" "$@"
+"$SCRIPT_DIR/build_cpu.sh" "$@"
